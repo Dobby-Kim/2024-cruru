29631de2ecbed61f93bd62b8f678b9f16392fcad
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2024-08-02 10:39:35 +0900

    chore(Actions-CI): Lint 자동화 workflow test를 위한 format에 맞지 않는 java 코드 생성

--- backend/src/main/java/com/cruru/applyform/service/ApplyFormService.java
+++ backend/src/main/java/com/cruru/applyform/service/ApplyFormService.java
@@ -27,17 +27,13 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ApplyFormService {
 
-    private static final String APPLY_FORM_BASE_URL = "www.cruru.kr/applyform/";
-
-    private final ApplicantRepository applicantRepository;
-    private final AnswerRepository answerRepository;
-    private final QuestionRepository questionRepository;
+    private static final String APPLY_FORM_BASE_URL = "www.cruru.kr/applyform/";private final ApplicantRepository applicantRepository;
+    private final AnswerRepository answerRepository;private final QuestionRepository questionRepository;
     private final ApplyFormRepository applyFormRepository;
     private final ProcessRepository processRepository;
 
-    @Transactional
-    public ApplyForm create(ApplyFormCreateRequest request, Dashboard createdDashboard) {
-        ApplyForm applyForm = toApplyForm(request, createdDashboard);
+    @Transactional public ApplyForm create(ApplyFormCreateRequest request, Dashboard createdDashboard) {
+            ApplyForm applyForm = toApplyForm(request, createdDashboard);
 
         ApplyForm savedApplyForm = applyFormRepository.save(applyForm);
         Long savedId = savedApplyForm.getId();
@@ -47,26 +43,21 @@ public class ApplyFormService {
         return savedApplyForm;
     }
 
-    private ApplyForm toApplyForm(ApplyFormCreateRequest request, Dashboard createdDashboard) {
-        return new ApplyForm(
-                request.title(),
-                request.postingContent(),
-                request.startDate(),
-                request.dueDate(),
-                createdDashboard
+    private ApplyForm toApplyForm(
+            ApplyFormCreateRequest request, Dashboard createdDashboard) {
+        return new ApplyForm(request.title(), request.postingContent(), request.startDate(), request.dueDate(), createdDashboard
         );
     }
 
-    public void submit(ApplyFormSubmitRequest request, long applyFormId) {
+    public void submit(ApplyFormSubmitRequest request, long applyFormId)
+    {
         validatePersonalDataCollection(request);
 
-        ApplyForm applyForm = applyFormRepository.findById(applyFormId)
+            ApplyForm applyForm = applyFormRepository.findById(applyFormId)
                 .orElseThrow(ApplyFormNotFoundException::new);
 
         Process firstProcess = processRepository.findFirstByDashboardIdOrderBySequenceAsc(
-                        applyForm.getDashboard().getId()
-                )
-                .orElseThrow(InternalServerException::new);
+                        applyForm.getDashboard().getId()).orElseThrow(InternalServerException::new);
 
         Applicant applicant = applicantRepository.save(
                 new Applicant(
@@ -78,15 +69,14 @@ public class ApplyFormService {
                 )
         );
 
-        for (AnswerCreateRequest answerCreateRequest : request.answerCreateRequest()) {
+        for (AnswerCreateRequest answerCreateRequest : request.answerCreateRequest())
             saveAnswerReplies(answerCreateRequest, applicant);
-        }
+
     }
 
     private void validatePersonalDataCollection(ApplyFormSubmitRequest request) {
-        if (!request.personalDataCollection()) {
-            throw new PersonalDataProcessingException();
-        }
+        if (!request.personalDataCollection()) throw new PersonalDataProcessingException();
+
     }
 
     private void saveAnswerReplies(AnswerCreateRequest answerCreateRequest, Applicant applicant) {
